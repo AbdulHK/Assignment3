9fd6e9d530ec2c1708800647ca1f207a89067cdc
dody1414 <D12122837@mydit.ie>
2015-04-04 21:44:41 +0100

    background

--- src/ie/dit/Main.java
+++ src/ie/dit/Main.java
@@ -23,7 +23,22 @@ public class Main extends PApplet {
 	{
 
 		background(175, 220, 255);
+	background(175, 220, 255);
 
+		// clouds
+		fill(255, 255, 255);
+		ellipse(40, 100, 50, 50);
+		ellipse(100, 100, 100, 100);
+		ellipse(150, 100, 50, 50);
+		ellipse(180, 100, 30, 30);
+		ellipse(220, 100, 10, 10);
+
+		fill(255, 255, 255);
+		ellipse(340, 100, 50, 50);
+		ellipse(380, 60, 80, 80);
+		ellipse(400, 100, 80, 80);
+		ellipse(450, 120, 50, 50);
+		ellipse(480, 100, 30, 30);
 		if (start)
 			{
 
