13e3e102c7dfa7c9e9b1f4256fcfd80415bfd6e5
dody1414 <D12122837@mydit.ie>
2015-04-16 12:46:01 +0100

    added stuff

--- src/ie/dit/Bird.java
+++ src/ie/dit/Bird.java
@@ -1,31 +1,37 @@
 package ie.dit;
 
-public class Bird {
+public class Bird
+ {
 
 	float speed, x, y;
 	boolean cashed = false;
 	Main parent; // The parent PApplet that we will render ourselves onto
 
-	Bird(Main p) {
+	Bird(Main p)
+	{
 		parent = p;
 		x = 300;
 		y = 300;
 	}
 
-	void move() {
+	void move()
+	{
 		y += speed;
 		parent.moveBird();
 	}
 
-	void jump() {
+	void jump() 
+	{
 		speed = -9;
 	}
 
-	void drag() {
+	void drag() 
+	{
 		speed += 0.4;
 	}
 
-	void drawBird() {
+	void drawBird()
+	{
 		parent.stroke(255);
 		parent.fill(255, 240, 85);
 		parent.strokeWeight(2);
@@ -39,22 +45,27 @@ public class Bird {
 		parent.ellipse(x + 12, y - 10, 15, 5);
 	}
 
-	void checkClash() {
+	void checkClash()
+	{
 
-		if (y > 500) {
+		if (y > 500) 
+		{
 			parent.setStop(true);
 		}
 
-		if (y < 0) {
+		if (y < 0) 
+		{
 			parent.setStop(true);
 		}
 
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++)
+			{
 
 			if ((x < parent.getPipe(i).xPos + 75 
 					&& x > parent.getPipe(i).xPos - 15)
 					&& (y < parent.getPipe(i).clearance -  (parent.getPipe(i).gap) 
-					|| y > parent.getPipe(i).clearance + 80)) {
+					|| y > parent.getPipe(i).clearance + 80)) 
+					{
 
 				parent.setStop(true);
 			}
